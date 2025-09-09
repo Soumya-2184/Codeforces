6b46d4ea7c8d39ee989462df8f43c906f77d664a
Soumya Gupta <118299378+Soumya-2184@users.noreply.github.com>
2025-09-09 19:16:28 +0530

    Add files via upload

---
+++ easyProblem.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        //int cnt=0;
+        //helper(n,1,)
+        cout<<n-1<<endl;
+
+    }
+    return 0;
+}              
